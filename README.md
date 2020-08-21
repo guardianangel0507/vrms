154c24a79fe957936063665fd8c8176f1ffffaea
Richard Brooks <guardian.angel0507@gmail.com>
2020-08-21 16:11:36 +0530

    Readme's Updated

--- README.md
+++ README.md
@@ -2,5 +2,3 @@
 
 This is a ReadMe Guide to describe everything happening with the project.
 Inside each folder you will find a main Readme.md file and Readme.md files for each files in respective folders with the file names.
-
-### ALL THE BEST.
--- index.php
+++ index.php
@@ -4,9 +4,9 @@ include_once "config/init.php";
 // echo SITE_ADDR;
 
 // Index Template Rendering
-//$indexTemplate = new Template("temp/index.html");
-// $indexTemplate = new Template(SITE_ROOT."public/lander.html");
-$indexTemplate = new Template("temp/tester.php");
+//$indexTemplate = new Template(SITE_ROOT."temp/index.html");
+$indexTemplate = new Template(SITE_ROOT . "public/lander.html");
+//$indexTemplate = new Template(SITE_ROOT."temp/tester.php");
 
 $indexTemplate->title = "VRMS - Manage Your Whole Ride With Us!";
 
