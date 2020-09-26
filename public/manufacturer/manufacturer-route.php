3ba5e0f395bc2c6d747729051b65a2c7cef3e470
Richard Brooks <guardian.angel0507@gmail.com>
2020-09-26 20:10:37 +0530

    Routing Enabled

--- public/manufacturer/manufacturer-route.php
+++ public/manufacturer/manufacturer-route.php
@@ -10,3 +10,19 @@ $userDetail = isset($_SESSION['userDetail']) ? $_SESSION['userDetail'] : null;
 $uri = getFinalUrl($_SERVER['REQUEST_URI']);
 
 echo $uri;
+
+switch ($uri) {
+    case "manufacturer":
+        $manufacPanelIndex = new Template(SITE_ROOT . "public/manufacturer/manufac-panel.php");
+        $manufacPanelIndex->title = "Manufacturer Panel";
+        $manufacPanelIndex->userData = isset($userData) ? $userData : null;
+        $manufacPanelIndex->userDetail = $userDetail;
+        $manufacPanelIndex->authNav = '<li class="nav-item">
+                    <a href="' . SITE_URL . 'public/manufacturer"
+                       class="nav-link">Manufacturer Panel</a>
+                </li>';
+        echo $manufacPanelIndex;
+        break;
+    default:
+        null;
+}
